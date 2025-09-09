f5a64b00c983df4efc35615f9ded8bba637ea21a
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-09 10:49:02 -0300

    RouterLinks gifs-side-menu with font awesome

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
-  styleUrl: './gifs-side-menu-options.component.css'
 })
 export class GifsSideMenuOptionsComponent {
-
+  menuOptions: MenuOption[] = [ // esto se puede convertir a señal y así sería mucho más eficiente.
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    },
+  ]
 }
